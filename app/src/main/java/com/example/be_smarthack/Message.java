31578945f5c6e4cc86f42e54c9aa5c95aeacd39d
Amari Garrett <amariwgarrett@gmail.com>
2020-10-10 10:03:27 -0400

    - Allow user to send messages

--- app/src/main/java/com/example/be_smarthack/Message.java
+++ app/src/main/java/com/example/be_smarthack/Message.java
@@ -1,12 +1,18 @@
 package com.example.be_smarthack;
 
+import java.util.Date;
+
 class Message {
     private String message;
     private boolean isChatBot;
+    private Date timeStamp;
 
-    public Message(String message, boolean isChatBot) {
+    public Message() {}
+
+    public Message(String message, boolean isChatBot, Date date) {
         this.setMessage(message);
         this.setIsChatBot(isChatBot);
+        this.setDate(date);
     }
 
     public String getText() {
@@ -23,5 +29,13 @@ class Message {
 
     public void setIsChatBot(boolean chatBot) {
         isChatBot = chatBot;
+    }
+
+    public Date getDate() {
+        return timeStamp;
+    }
+
+    public void setDate(Date timeStamp) {
+        this.timeStamp = timeStamp;
     }
 }
--- app/src/main/java/com/example/be_smarthack/MessageListActivity.java
+++ app/src/main/java/com/example/be_smarthack/MessageListActivity.java
@@ -5,31 +5,55 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.be_smarthack.databinding.ActivityMessageListBinding;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Calendar;
 
 public class MessageListActivity extends AppCompatActivity {
     private RecyclerView mMessageRecycler;
     private MessageListAdapter mMessageAdapter;
 
+    ActivityMessageListBinding mBinding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_message_list);
+        mBinding = ActivityMessageListBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
 
-        ArrayList<Message> messageList = new ArrayList<>();
+        final ArrayList<Message> messageList = new ArrayList<>();
 
-        messageList.add(new Message("Hello", false));
-        messageList.add(new Message("How are you?", false));
-        messageList.add(new Message("I'm great!", true));
+        messageList.add(new Message("Hello", false, Calendar.getInstance().getTime()));
+        messageList.add(new Message("How are you?", false, Calendar.getInstance().getTime()));
+        messageList.add(new Message("I'm great!", true, Calendar.getInstance().getTime()));
 
         mMessageAdapter = new MessageListAdapter(this, messageList);
-        mMessageRecycler = (RecyclerView) findViewById(R.id.reyclerview_message_list);
+        mMessageRecycler = mBinding.reyclerviewMessageList;
         mMessageRecycler.setAdapter(mMessageAdapter);
 
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         layoutManager.setStackFromEnd(true);
         mMessageRecycler.setLayoutManager(layoutManager);
+
+        mBinding.buttonChatboxSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Message newMessage = new Message();
+                newMessage.setMessage(mBinding.edittextChatbox.getText().toString());
+                newMessage.setIsChatBot(false);
+                newMessage.setDate(Calendar.getInstance().getTime());
+
+                mBinding.edittextChatbox.setText("");
+                messageList.add(newMessage);
+                mMessageAdapter.notifyDataSetChanged();
+                mMessageRecycler.smoothScrollToPosition(messageList.size());
+
+                messageList.add(new Message("Okay. Here are your in-flight options.", true, Calendar.getInstance().getTime()));
+                mMessageAdapter.notifyDataSetChanged();
+            }
+        });
     }
 }
