6fcd3005151d1ef145ef1d2b39954d546641180c
Amari Garrett <amariwgarrett@gmail.com>
2020-10-10 03:40:24 -0400

    Initial home screen with Shuri FAB button

--- app/src/main/java/com/example/be_smarthack/MainActivity.java
+++ app/src/main/java/com/example/be_smarthack/MainActivity.java
@@ -1,14 +1,37 @@
 package com.example.be_smarthack;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import com.example.be_smarthack.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    final FragmentManager fragmentManager = getSupportFragmentManager();
+
+    Fragment mActiveFragment;
+
+    ActivityMainBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_main);
+
+        replaceFragment(new HomeFragment());
+    }
+
+    public void replaceFragment(Fragment fragment) {
+        fragmentManager
+                .beginTransaction()
+                .replace(mBinding.containerFrameLayout.getId(), fragment)
+                .addToBackStack(null)
+                .commit();
     }
 }
